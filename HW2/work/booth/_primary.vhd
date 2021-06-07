fc8e0c3af21d7836efcf6abe4a9171944d135145
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2021-06-07 18:11:36 +0800

    Delete HW2 directory

--- HW2/work/booth/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity booth is
-    port(
-        \out\           : out    vl_logic_vector(11 downto 0);
-        in1             : in     vl_logic_vector(5 downto 0);
-        in2             : in     vl_logic_vector(5 downto 0)
-    );
-end booth;
